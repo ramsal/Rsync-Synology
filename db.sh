d29bcffb95097f373795a76cb349183da5c09bc6
Ramón Salado <ramon@outhink.es>
2019-07-11 18:30:43 +0200

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+USER="myuser"
+KEY="/home/user/.ssh/mykey"
+PORT="22"
+SERVER="domain.tld"
+TARGET="/home/user/backup/database"
+DB_USER="user"
+DB_PASSWORD="pass"
+DATABASE="db"
+ssh -p ${PORT} -i ${KEY} ${USER}@${SERVER} "mysqldump -u ${DB_USER} -p${DB_PASSWORD} ${DATABASE}" > "${TARGET}/${DATABASE}.sql"
