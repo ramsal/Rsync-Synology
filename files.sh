46fbac133770768dff746c1f2e90f149b27cb6f6
Ramón Salado <ramon@outhink.es>
2019-07-11 18:30:06 +0200

    Create files.sh

---
+++ files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#ramsal
+
+USER="myuser"
+KEY="/home/user/.ssh/mykey"
+PORT="22"
+SERVER="domain.tld"
+SOURCE="/home/service/"
+TARGET="/home/user/backup/data"
+LOG="/home/user/backup/$current_date.log"
+
+rsync -avz -L --progress -e "ssh -i ${KEY} -p $PORT" $USER@$SERVER:$SOURCE $TARGET >> $LOG 2>&1
